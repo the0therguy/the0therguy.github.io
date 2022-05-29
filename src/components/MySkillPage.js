a38223b3a2ffd50e6ef08bac3909798a8b7f7cff
Mohammad BakiBillah Chowdhuryy (Ifty) <nblues399@gmail.com>
2022-05-30 02:11:22 +0600

    skill page touched.

--- src/components/MySkillPage.js
+++ src/components/MySkillPage.js
@@ -1,10 +1,61 @@
 import React from "react";
+import styled, {ThemeProvider} from "styled-components";
+import {lightTheme} from "./Themes";
+import {Design, Develope} from "./AllSvgs";
+
+const Box = styled.div`
+  background-color: ${props => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+const Main = styled.div`
+  border: 2px solid ${props => props.theme.text};
+  color: ${props => props.theme.text};
+  background-color: ${props => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  
+  font-family: 'Ubuntu Mono',monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Title = styled.h2`
+    display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
+
+`
 
 const MySkillPage = () => {
     return (
-        <div>
-            Skill Page component
-        </div>
+
+            <ThemeProvider theme={lightTheme}>
+                <Box>
+                    <Main>
+                        <Title>
+                            <Design width={40} height={40}/>
+                            Designer
+
+                        </Title>
+                    </Main>
+                    <Main>
+
+                    </Main>
+                </Box>
+
+            </ThemeProvider>
+
     )
 
 }
